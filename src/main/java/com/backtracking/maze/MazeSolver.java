11b49d97f81ae87d520bf71f5c72304889aa93cb
Sebastian Leal <sebastian.leal@globant.com>
2020-02-13 10:24:22 -0500

    added solve maze with backtracking

--- src/main/java/com/backtracking/maze/MazeSolver.java
+++ src/main/java/com/backtracking/maze/MazeSolver.java
@@ -2,16 +2,63 @@ package com.backtracking.maze;
 
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 /**
+ * Class that finds the path in a Maze.
+ *
  * @author sebastian.leal
  */
 public class MazeSolver {
 
-    public List<Integer> findPath(int startPosition, int endPosition, Map<Integer, Set<Integer>> maze) {
-        return Collections.emptyList();
+    private Set<Integer> visitedNodes;
+    private List<Integer> path;
+
+    /**
+     * Solve the path implementing Backtracking (find one way).
+     * <p>
+     * It may not be the best possible solution.
+     *
+     * @param startPosition int with the start Position
+     * @param endPosition   int with the end Position
+     * @param maze          Map<Integer,Set<Integer>> map with the maze.
+     * @return List<Integer> with the solution.
+     */
+    public List<Integer> solve(int startPosition, int endPosition, Map<Integer, Set<Integer>> maze) {
+        visitedNodes = new HashSet<>();
+        path = new LinkedList<>();
+        solvePath(startPosition, endPosition, maze);
+        Collections.reverse(path);
+        return path;
+    }
+
+    /**
+     * Recursive method to find the path between two nodes.
+     *
+     * @param startPosition int with the start Position
+     * @param endPosition   int with the end Position
+     * @param maze          Map<Integer,Set<Integer>> map with the maze.
+     * @return boolean, true if the path from start to end was found, false otherwise.
+     */
+    private boolean solvePath(int startPosition, int endPosition, Map<Integer, Set<Integer>> maze) {
+        if (!visitedNodes.contains(startPosition)) {
+            for (Integer edge : maze.get(startPosition)) {
+                if ((edge == endPosition)) {
+                    path.add(endPosition);
+                    path.add(startPosition);
+                    return true;
+                }
+                if (solvePath(edge, endPosition, maze)) {
+                    path.add(startPosition);
+                    return true;
+                }
+            }
+        }
+        visitedNodes.add(startPosition);
+        return false;
     }
 }
--- src/test/java/com/backtracking/maze/MazeSolverTest.java
+++ src/test/java/com/backtracking/maze/MazeSolverTest.java
@@ -2,7 +2,7 @@ package com.backtracking.maze;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -11,9 +11,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class MazeSolverTest {
+public class MazeSolverTest {
 
     private MazeSolver mazeSolver;
 
@@ -23,13 +21,32 @@ class MazeSolverTest {
     }
 
     @Test
-    void findPath() {
+    public void findPath() {
         int startPosition = 0;
         int endPosition = 8;
-        List<Integer> path = mazeSolver.findPath(startPosition, endPosition, createMaze3x3());
+        List<Integer> path = mazeSolver.solve(startPosition, endPosition, createMaze3x3());
         Assert.assertEquals(Stream.of(0, 3, 4, 7, 8).collect(Collectors.toList()), path);
     }
 
+    @Test
+    public void findPath_2() {
+        int startPosition = 0;
+        int endPosition = 8;
+        List<Integer> path = mazeSolver.solve(startPosition, endPosition, createMaze3x3_double_path());
+        Assert.assertEquals(Stream.of(0, 3, 4, 5, 8).collect(Collectors.toList()), path);
+    }
+
+    /**
+     * 0 -> 1 -> 2
+     * |
+     * v
+     * 3 -> 4 -> 5
+     * |    |
+     * v    v
+     * 6    7 -> 8
+     *
+     * @return
+     */
     private HashMap<Integer, Set<Integer>> createMaze3x3() {
         HashMap<Integer, Set<Integer>> maze = new HashMap<>();
         maze.put(0, Stream.of(1, 3).collect(Collectors.toSet()));
@@ -38,6 +55,31 @@ class MazeSolverTest {
         maze.put(3, Stream.of(4, 6).collect(Collectors.toSet()));
         maze.put(4, Stream.of(5, 7).collect(Collectors.toSet()));
         maze.put(5, Collections.emptySet());
+        maze.put(6, Collections.emptySet());
+        maze.put(7, Stream.of(8).collect(Collectors.toSet()));
+        maze.put(8, Collections.emptySet());
+        return maze;
+    }
+
+    /**
+     * 0 -> 1 -> 2
+     * |
+     * v
+     * 3 -> 4 -> 5
+     * |    |    |
+     * v    v    v
+     * 6    7 -> 8
+     *
+     * @return
+     */
+    private HashMap<Integer, Set<Integer>> createMaze3x3_double_path() {
+        HashMap<Integer, Set<Integer>> maze = new HashMap<>();
+        maze.put(0, Stream.of(1, 3).collect(Collectors.toSet()));
+        maze.put(1, Stream.of(2).collect(Collectors.toSet()));
+        maze.put(2, Collections.emptySet());
+        maze.put(3, Stream.of(4, 6).collect(Collectors.toSet()));
+        maze.put(4, Stream.of(5, 7).collect(Collectors.toSet()));
+        maze.put(5, Stream.of(8).collect(Collectors.toSet()));
         maze.put(6, Collections.emptySet());
         maze.put(7, Stream.of(8).collect(Collectors.toSet()));
         maze.put(8, Collections.emptySet());
